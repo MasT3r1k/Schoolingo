309a79c6d52adc6637a6d7f165702b6c068ddefa
MasT3r1k <master0123@centrum.cz>
2024-10-26 12:45:17 +0200

    Settings update

--- src/app/board/User/settings/settings.component.ts
+++ src/app/board/User/settings/settings.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { TabsComponent } from '@Components/Tabs/Tabs';
 import { Schoolingo } from '@Schoolingo';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [TabsComponent, NgClass],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css', '../../../Styles/card.css']
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
+  public selectedTab: BehaviorSubject<number> = new BehaviorSubject(0);
+  public options: string[] = ['changepassword', 'language', 'theme'];
+
   constructor(
     public schoolingo: Schoolingo
   ) {}
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
 }
