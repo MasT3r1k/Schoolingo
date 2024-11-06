1a8921223720d39126f93e9a9b75bd80750f044c
MasT3r1k <master0123@centrum.cz>
2024-11-06 22:07:45 +0100

    Update modules

--- src/app/board/main/main.component.ts
+++ src/app/board/main/main.component.ts
@@ -1,12 +1,28 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Dropdown } from '@Components/Dropdowns/Dropdown';
+import { TabsComponent } from '@Components/Tabs/Tabs';
+import { Locale } from '@Schoolingo/Locale';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, TabsComponent, NgClass],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrls: ['./main.component.css', '../../Styles/item.css']
 })
 export class MainComponent {
+  constructor(
+    public locale: Locale,
+    public dropdown: Dropdown
+  ) {}
+
+
+  // Timetable module
+  public timetableSelectedTab: BehaviorSubject<number> = new BehaviorSubject(0);
+  public timetableOptionsName: string = 'timetableOptions';
+  
 
 }
