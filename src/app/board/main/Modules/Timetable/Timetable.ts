1fba8043a6b186054c6fd48d764606fdab39e203
MasT3r1k <master0123@centrum.cz>
2024-11-10 22:52:32 +0100

    Timetable update on main page

--- src/app/board/main/Modules/Timetable/Timetable.ts
+++ src/app/board/main/Modules/Timetable/Timetable.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Schoolingo } from '@Schoolingo';
+import { user } from '@Schoolingo/User';
+import moment from 'moment';
 
 @Component({
   host: {'module': 'timetable'},
@@ -7,6 +10,25 @@ import { Component } from '@angular/core';
   templateUrl: './Timetable.html',
   styleUrl: './Timetable.css'
 })
-export class TimetableComponent {
+export class TimetableComponent implements OnInit {
+  constructor(
+    public schoolingo: Schoolingo
+  ) {}
+
+  public day: moment.Moment = moment().add(1, 'day');
+
+  ngOnInit(): void {
+
+    // Get timetable
+    let user: user | null = this.schoolingo.userService.getUser();
+    let userId = user?.id;
+
+    if (user && user.type == 'parent') {
+      userId = this.schoolingo.userService.children[this.schoolingo.userService.selectedChild].personId;
+    }
+    this.schoolingo.socketService.emit('timetable:getLessons', { userId, week: this.day.isoWeek(), year: this.day.year() });
+
+    console.log(this.day.day())
+  }
 
 }
