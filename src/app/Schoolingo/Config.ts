22db1d95848c247037bea5265926a65bffdfc2cc
MasT3r1k <master0123@centrum.cz>
2024-11-10 10:22:33 +0100

    Updated config for auto detect dev or production

--- src/app/Schoolingo/Config.ts
+++ src/app/Schoolingo/Config.ts
@@ -2,6 +2,6 @@ export const name = 'Schoolingo';
 export const version = '2.0';
 
 // NO DEV: /api/
-export const api = 'http://localhost:8888/api/';
-export const socketIP = 'http://localhost:8888';
+export const api = (window.location.hostname == "localhost") ? 'http://localhost:8888/api/' : '/api/';
+export const socketIP = (window.location.hostname == "localhost") ? 'http://localhost:8888' : window.location.host;
 export const localeURL = '/locales/';
