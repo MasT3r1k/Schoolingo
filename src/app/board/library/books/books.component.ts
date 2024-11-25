41f5d1a4bcce48b1e3dd14a936e4cd2eb05477ae
MasT3r1k <master0123@centrum.cz>
2024-11-25 22:20:24 +0100

    Added books page

---
+++ src/app/board/library/books/books.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css', '../../../Styles/card.css']
+})
+export class BooksComponent {
+
+}
