e4c389a55c738679598a122ff62947320bea6543
MasT3r1k <master0123@centrum.cz>
2024-11-25 22:21:08 +0100

    Added persons page

---
+++ src/app/board/persons/persons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './persons.component.html',
+  styleUrl: './persons.component.css'
+})
+export class PersonsComponent {
+
+}
