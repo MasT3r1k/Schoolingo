f8f7a7b30ae0566285c84da378f2a2c5e6153a4b
MasT3r1k <master0123@centrum.cz>
2024-11-25 22:26:05 +0100

    Added datalist component

---
+++ src/app/Components/Datalist/Datalist.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'schoolingo-datalist',
+    templateUrl: './Datalist.html',
+    standalone: true,
+    styleUrl: './Datalist.css'
+})
+export class DatalistComponent implements OnInit {
+    
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    ngOnDestroy(): void {}
+}
