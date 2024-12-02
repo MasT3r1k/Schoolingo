d1be2594ec127cd82a690240f3769687627adf6a
MasT3r1k <master0123@centrum.cz>
2024-12-02 22:51:32 +0100

    Traineeship update

--- src/app/board/Traineeship/companies/companies.component.ts
+++ src/app/board/Traineeship/companies/companies.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Data, dataAPI, DatalistComponent, Metadata } from '@Components/Datalist/Datalist';
+import { Schoolingo } from '@Schoolingo';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-companies',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, DatalistComponent],
   templateUrl: './companies.component.html',
-  styleUrl: './companies.component.css'
+  styleUrls: ['../../../Styles/card.css', '../../../Styles/input.css', './companies.component.css']
 })
-export class CompaniesComponent {
+export class CompaniesComponent implements OnInit {
+  constructor(
+    public schoolingo: Schoolingo
+  ) {}
+
+  public companies: BehaviorSubject<Data[][] | any> = new BehaviorSubject([]);
+  public search = new FormControl();
+
+  public metadata: Metadata = {
+    rows: 0
+  };
+
+
+  datalist: DatalistComponent | null = null;
+  receivedDatalist(value: DatalistComponent): void {
+    this.datalist = value;
+  }
+
+  ngOnInit(): void {
+    this.schoolingo.socketService.addFunction("traineeship:getCompanies").subscribe((data: dataAPI) => {
+      let companiesList: Data[][] = []
+      console.log(data);
+      data.data.forEach((company: any) => {
+        companiesList.push([{ id: company.companyId }, {value: company.name, isLocale: false}, {value: company.addressOffice, isLocale: false}, {value: company.CIN, isLocale: false}, {value: company.web, isLocale: false}])
+      })
+      this.metadata.rows = data.rows;
+      this.companies.next(companiesList);
+    });
+  }
 
 }
