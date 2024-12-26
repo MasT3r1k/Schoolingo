f281c3431a7e1b6df4aa4e94b2fdf16dd09dc807
MasT3r1k <master0123@centrum.cz>
2024-12-26 14:53:30 +0100

    Homework update

--- src/app/board/Teach/homeworks/homeworks.component.ts
+++ src/app/board/Teach/homeworks/homeworks.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { TabsComponent } from '@Components/Tabs/Tabs';
+import { Schoolingo } from '@Schoolingo';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: 'app-homeworks',
   standalone: true,
-  imports: [],
+  imports: [TabsComponent],
   templateUrl: './homeworks.component.html',
-  styleUrl: './homeworks.component.css'
+  styleUrls: ['./homeworks.component.css', '../../../Styles/card.css']
 })
 export class HomeworksComponent {
-
+  public selectedTab: BehaviorSubject<number> = new BehaviorSubject(0);
+  constructor(
+    public schoolingo: Schoolingo
+  ) {}
 }
