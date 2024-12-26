52a05c727f47b17d2c2acac1a2428a8e14990125
MasT3r1k <master0123@centrum.cz>
2024-12-26 14:53:15 +0100

    Config updated

--- src/app/Schoolingo/Config.ts
+++ src/app/Schoolingo/Config.ts
@@ -1,5 +1,5 @@
 export namespace Config {
-    export const API_URL = 'http://localhost:8888/api/';
-    export const socketIP =  'http://localhost:8888';
+    export const socketIP = 'http://localhost:8888';
+    export const API_URL = socketIP + '/api/';
     export const localeURL = '/locales/';
 }
