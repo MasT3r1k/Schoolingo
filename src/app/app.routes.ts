b5e6326dbe877e5412cfbf655f85a979d0338ec9
MasT3r1k <master0123@centrum.cz>
2025-01-14 21:04:01 +0100

    All components to lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,58 +2,7 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './Auth/auth.component';
 import { NotUserGuard, UserGuard } from '@Guards/Auth.guard';
 import { BoardComponent } from './board/board.component';
-import { MainComponent as BoardMain } from './board/main/main.component';
-import { TimetableComponent as TeachTimetable } from './board/Teach/timetable/timetable.component';
-import { HomeworksComponent as TeachHomeworks } from './board/Teach/homeworks/homeworks.component';
-import { AbsenceComponent as TeachAbsence } from './board/Teach/absence/absence.component';
-import { SubstitutionComponent as TeachSubstitution } from './board/Teach/substitution/substitution.component';
-import { TutoringComponent as TeachTutoring } from './board/Teach/tutoring/tutoring.component';
-import { ClassbookComponent as TeachClassbook } from './board/Teach/classbook/classbook.component';
-import { SubjectsComponent as TeachSubjects } from './board/Teach/subjects/subjects.component';
-import { ProfileComponent as UserProfile } from './board/User/profile/profile.component';
-import { LoginHistoryComponent as UserLoginHistory } from './board/User/login-history/login-history.component';
-import { DevicesComponent as UserDevices } from './board/User/devices/devices.component';
-import { SettingsComponent as UserSettings } from './board/User/settings/settings.component';
-import { SendComponent as MessagesSend } from './board/Messages/send/send.component';
-import { ReceivedComponent as MessagesReceived } from './board/Messages/received/received.component';
-import { SentComponent as MessagesSent } from './board/Messages/sent/sent.component';
-import { GroupsComponent as MessagesGroups } from './board/Messages/groups/groups.component';
-import { NoticeboardComponent as MessagesNoticeboard } from './board/Messages/noticeboard/noticeboard.component';
 import { Error404Component } from './board/Errors/error404/error404.component';
-import { IntermComponent as MarksInterm } from './board/Marks/interm/interm.component';
-import { MidtermComponent as MarksMidterm } from './board/Marks/midterm/midterm.component';
-import { IntermRecordComponent as MarksIntermRecord } from './board/Marks/interm-record/interm-record.component';
-import { studentsComponent as Students } from './board/students/students.component';
-import { TeachersComponent as Teachers } from './board/teachers/teachers.component';
-import { ManageUsersComponent as Users } from './board/manage-users/manage-users.component';
-import { DiscordConnectComponent } from './board/discord-connect/discord-connect.component';
-import { LoansComponent as LibraryLoans } from './board/library/loans/loans.component';
-import { BooksComponent as LibraryBooks } from './board/library/books/books.component';
-import { OverviewComponent as TraineeshipOverview } from './board/Traineeship/overview/overview.component';
-import { DiaryComponent as TraineeshipDiary } from './board/Traineeship/diary/diary.component';
-import { CompaniesComponent as TraineeshipCompanies } from './board/Traineeship/companies/companies.component';
-import { ManageComponent as TraineeshipManage } from './board/Traineeship/manage/manage.component';
-import { OrderComponent as CanteenOrder } from './board/Canteen/order/order.component';
-import { DispensingComponent as CanteenDispensing } from './board/Canteen/dispensing/dispensing.component';
-import { MealsComponent as CanteenMeals } from './board/Canteen/meals/meals.component';
-import { SettingsComponent as CanteenSettings } from './board/Canteen/settings/settings.component';
-import { ClassFundComponent as PaymentClassFund } from './board/payments/class-fund/class-fund.component';
-import { GraduateClassFundComponent as PaymentGraduateClassFund } from './board/payments/graduate-class-fund/graduate-class-fund.component';
-import { ListingComponent as PaymentListing } from './board/payments/listing/listing.component';
-import { UnaccountedDocumentsComponent as PaymentUnaccountedDocuments } from './board/payments/unaccounted-documents/unaccounted-documents.component';
-import { DocumentsComponent as PaymentDocuments } from './board/payments/documents/documents.component';
-import { RegularPaymentsComponent as PaymentRegularPayments } from './board/payments/regular-payments/regular-payments.component';
-import { NewPaymentComponent as PaymentNewPayment } from './board/payments/new-payment/new-payment.component';
-import { NewDepositComponent as PaymentNewDeposit } from './board/payments/new-deposit/new-deposit.component';
-import { AccountsComponent as PaymentAccounts } from './board/payments/accounts/accounts.component';
-import { SettingsComponent as PaymentSettings } from './board/payments/settings/settings.component';
-import { SystemComponent } from './board/system/system.component';
-import { SettingsComponent as ToolsSettings } from './board/Tools/settings/settings.component';
-import { ManageUsersComponent as ToolsManageUsers } from './board/Tools/manage-users/manage-users.component';
-import { DocumentsComponent } from './board/documents/documents.component';
-import { CalendarComponent } from './board/calendar/calendar.component';
-import { EducationMeasuresComponent as MarksEducationMeasures } from './board/Marks/education-measures/education-measures.component';
-import { VehiclesComponent as FleetVehiclesVehicles } from './board/fleetVehicles/vehicles/vehicles.component';
 
 export const routes: Routes = [
     {
@@ -159,23 +108,28 @@ export const routes: Routes = [
           children: [
             {
               path: 'send',
-              loadComponent: () => import('./board/Messages/send/send.component').then(m => m.SendComponent)
+              loadComponent: () => import('./board/Messages/send/send.component')
+                                    .then(m => m.SendComponent)
             },
             {
               path: 'received',
-              loadComponent: () => import('./board/Messages/received/received.component').then(m => m.ReceivedComponent)
+              loadComponent: () => import('./board/Messages/received/received.component')
+                                    .then(m => m.ReceivedComponent)
             },
             {
               path: 'sent',
-              loadComponent: () => import('./board/Messages/sent/sent.component').then(m => m.SentComponent)
+              loadComponent: () => import('./board/Messages/sent/sent.component')
+                                    .then(m => m.SentComponent)
             },
             {
               path: 'groups',
-              loadComponent: () => import('./board/Messages/groups/groups.component').then(m => m.GroupsComponent)
+              loadComponent: () => import('./board/Messages/groups/groups.component')
+                                    .then(m => m.GroupsComponent)
             },
             {
               path: 'noticeboard',
-              loadComponent: () => import('./board/Messages/noticeboard/noticeboard.component').then(m => m.NoticeboardComponent)
+              loadComponent: () => import('./board/Messages/noticeboard/noticeboard.component')
+                                    .then(m => m.NoticeboardComponent)
             }
           ]
         },
@@ -225,7 +179,7 @@ export const routes: Routes = [
             {
               path: 'vehicles',
               loadComponent: () => import('./board/fleetVehicles/vehicles/vehicles.component')
-              .then(m => m.VehiclesComponent)
+                                    .then(m => m.VehiclesComponent)
             }
           ]
         },
@@ -233,34 +187,49 @@ export const routes: Routes = [
           path: 'payments',
           children: [
             {
-              path: 'classfund', component: PaymentClassFund
+              path: 'classfund',
+              loadComponent: () => import('./board/payments/class-fund/class-fund.component')
+                                    .then(m => m.ClassFundComponent)
             },
             {
-              path: 'graduateclassfund', component: PaymentGraduateClassFund
+              path: 'graduateclassfund',
+              loadComponent: () => import('./board/payments/graduate-class-fund/graduate-class-fund.component')
+                                    .then(m => m.GraduateClassFundComponent)
             },
             {
-              path: 'listing', component: PaymentListing
+              path: 'listing',
+              loadComponent: () => import('./board/payments/listing/listing.component')
+                                    .then(m => m.ListingComponent)
             },
             {
-              path: 'unaccounteddocuments', component: PaymentUnaccountedDocuments
+              path: 'unaccounteddocuments',
+              loadComponent: () => import('./board/payments/unaccounted-documents/unaccounted-documents.component')
+                                    .then(m => m.UnaccountedDocumentsComponent)
             },
             {
-              path: 'documents', component: PaymentDocuments
+              path: 'documents',
+              loadComponent: () => import('./board/payments/documents/documents.component')
+                                    .then(m => m.DocumentsComponent)
             },
             {
-              path: 'regularpayments', component: PaymentRegularPayments
+              path: 'regularpayments',
+              loadComponent: () => import('./board/payments/regular-payments/regular-payments.component')
+                                    .then(m => m.RegularPaymentsComponent)
             },
             {
-              path: 'newpayment', component: PaymentNewPayment
+              path: 'newpayment',
+              loadComponent: () => import('./board/payments/new-payment/new-payment.component')
+                                    .then(m => m.NewPaymentComponent)
             },
             {
-              path: 'newdeposit', component: PaymentNewDeposit
+              path: 'newdeposit',
+              loadComponent: () => import('./board/payments/new-deposit/new-deposit.component')
+                                    .then(m => m.NewDepositComponent)
             },
             {
-              path: 'accounts', component: PaymentAccounts
-            },
-            {
-              path: 'settings', component: PaymentSettings
+              path: 'accounts',
+              loadComponent: () => import('./board/payments/accounts/accounts.component')
+                                    .then(m => m.AccountsComponent)
             }
           ]
         },
@@ -268,41 +237,54 @@ export const routes: Routes = [
           path: 'user',
           children: [
             {
-              path: 'profile', component: UserProfile
+              path: 'profile',
+              loadComponent: () => import('./board/User/profile/profile.component')
+                                    .then(m => m.ProfileComponent)
             },
             {
-              path: 'loginhistory', component: UserLoginHistory
+              path: 'loginhistory',
+              loadComponent: () => import('./board/User/login-history/login-history.component')
+                                    .then(m => m.LoginHistoryComponent)
             },
             {
-              path: 'devices', component: UserDevices
+              path: 'devices',
+              loadComponent: () => import('./board/User/devices/devices.component')
+                                    .then(m => m.DevicesComponent)
             },
             {
-              path: 'settings', component: UserSettings
+              path: 'settings',
+              loadComponent: () => import('./board/User/settings/settings.component')
+                                    .then(m => m.SettingsComponent)
             }
           ]
         },
         {
           path: 'calendar',
-          component: CalendarComponent
+          loadComponent: () => import('./board/calendar/calendar.component')
+                                .then(m => m.CalendarComponent)
         },
         {
           path: 'documents',
-          component: DocumentsComponent
+          loadComponent: () => import('./board/documents/documents.component')
+                                .then(m => m.DocumentsComponent)
         },
         {
           path: 'system',
-          component: SystemComponent
-        },
+          loadComponent: () => import('./board/system/system.component')
+                                .then(m => m.SystemComponent)
+        },        
         {
           path: 'tools',
           children: [
             {
               path: 'settings',
-              component: ToolsSettings
+              loadComponent: () => import('./board/Tools/settings/settings.component')
+                                    .then(m => m.SettingsComponent)
             },
             {
               path: 'manageusers',
-              component: ToolsManageUsers
+              loadComponent: () => import('./board/Tools/manage-users/manage-users.component')
+                                    .then(m => m.ManageUsersComponent)
             }
           ]
         },
