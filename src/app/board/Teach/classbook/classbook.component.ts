9a0784f2db100e8584e12a53abceb500b1fb191d
MasT3r1k <master0123@centrum.cz>
2025-01-15 23:41:02 +0100

    Add calendar

--- src/app/board/Teach/classbook/classbook.component.ts
+++ src/app/board/Teach/classbook/classbook.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Dropdown } from '@Components/Dropdowns/Dropdown';
+import { Schoolingo } from '@Schoolingo';
+import { IconsModule } from '../../../Modules/Icons.module';
+import { BehaviorSubject } from 'rxjs';
+import moment from 'moment';
 
 @Component({
-  selector: 'app-classbook',
   standalone: true,
-  imports: [],
+  imports: [NgClass, IconsModule],
   templateUrl: './classbook.component.html',
-  styleUrl: './classbook.component.css'
+  styleUrls: ['./classbook.component.css', '../../../Styles/card.css', '../../../Styles/item.css']
 })
-export class ClassbookComponent {
+export class ClassbookComponent implements OnInit {
+  public calendarCalendarName = 'classbookCalendar';
+  public selectedDate: BehaviorSubject<moment.Moment> = new BehaviorSubject<moment.Moment>(moment());
+
+  constructor(
+    public schoolingo: Schoolingo,
+    public dropdown: Dropdown
+  ) {}
+
+  ngOnInit(): void {
+    // Calendar
+    this.dropdown.create(this.calendarCalendarName, { title: '', isOpen: false, items: [{
+      type: 'calendar',
+      date: this.selectedDate,
+      selectedMonth: this.selectedDate.getValue().clone(),
+      isActive: true
+    }] });
+
+    this.selectedDate.next(moment());
+  }
+
+    ngOnDestroy(): void {
+      this.selectedDate.next(moment());
+  
+      this.dropdown.remove(this.calendarCalendarName);
+    }
 
 }
