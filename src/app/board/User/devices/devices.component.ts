93c646744f840960d3c23f56a99dc5cfb38f281d
MasT3r1k <master0123@centrum.cz>
2025-03-16 23:38:14 +0100

    Confirm to logout all devices, added tooltip to logout specific device

--- src/app/board/User/devices/devices.component.ts
+++ src/app/board/User/devices/devices.component.ts
@@ -6,15 +6,18 @@ import { Subscription } from 'rxjs';
 import { Device } from './devices.component.d'; 
 import { Utils } from '@Schoolingo/Utils';
 import { IconsModule } from '../../../Modules/Icons.module';
+import { Alert } from '@Schoolingo/Alert';
 
 @Component({
   standalone: true,
   imports: [RouterLink, IconsModule],
   providers: [],
   templateUrl: './devices.component.html',
-  styleUrls: ['./devices.component.css', '../../../Styles/card.css', '../../../Styles/select.css', '../../../Styles/input.css']
+  styleUrls: ['./devices.component.css', '../../../Styles/card.css', '../../../Styles/select.css', '../../../Styles/input.css', '../../../Components/Modal/modal.css']
 })
 export class DevicesComponent {
+  public alert: Alert | null = null;
+  public modal: 'confirmLogout' | null = null;
   private devices: Device[] = [];
   private subscribers: Subscription[] = [];
   public country = Country;
@@ -36,6 +39,8 @@ export class DevicesComponent {
 
     this.subscribers.push(
       this.schoolingo.socketService.addFunction("devices:removeDevice").subscribe((data: { id: number[] }) => {
+        this.modal = null;
+        this.alert = null;
         for (let id of data.id) {
           let index = this.devices.findIndex((device: Device) => device.id == id);
           this.devices.splice(index, 1);
@@ -68,8 +73,15 @@ export class DevicesComponent {
   }
 
 
-  public removeAllDevices(): void {
-    if (!this.getAnotherDevices().length) return;
+  public removeAllDevices(force: boolean = false): void {
+    if (!this.getAnotherDevices().length) {
+      this.alert = new Alert('error', 'devices/noAnotherDevicesToLogout');
+      return;
+    }
+    if (!force) {
+      this.modal = 'confirmLogout';
+      return;
+    }
 
     let deviceIds: number[] = [];
     this.getAnotherDevices().forEach((device: Device) => {
