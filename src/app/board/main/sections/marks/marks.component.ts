f0aadcb70886363ef89a469d08f5cd28d3013965
MasT3r1k <master0123@centrum.cz>
2025-12-01 09:07:38 +0100

    Added loading marks

--- src/app/board/main/sections/marks/marks.component.ts
+++ src/app/board/main/sections/marks/marks.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { Authentication } from '@Schoolingo/authentication';
+import { Config } from '@Schoolingo/config';
 
 @Component({
-  selector: 'app-marks',
   imports: [],
   templateUrl: './marks.component.html',
   styleUrl: './marks.component.css'
 })
-export class MarksComponent {
+export class MarksComponent implements OnInit {
+  private http = inject(HttpClient);
+  public u = inject(Authentication);
+  public marks: any[] = [];
 
+  public loadMarks(): void {
+    this.http.post(
+      `${Config.API_URL}/v1/marks/student?limit10`,
+      { student_id: this.u.getId() },
+      { withCredentials: true }
+    )
+    .subscribe((data) => {
+      if ('marks' in data) {
+        this.marks = data.marks as any[];
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadMarks();
+    this.u.getAuthState()
+    .subscribe((data) => {
+      if (data) {
+        this.loadMarks();
+      }
+    })
+  }
 }
