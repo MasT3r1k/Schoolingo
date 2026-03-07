f501a24d0a6d64958c421091cd28967a19786e63
MasT3r1k <master0123@centrum.cz>
2026-03-07 11:56:51 +0100

    Added option to put content to file

--- src/app/board/documents/modals/create-file/create-file.component.ts
+++ src/app/board/documents/modals/create-file/create-file.component.ts
@@ -17,32 +17,33 @@ export class CreateFileComponent {
   private http = inject(HttpClient);
   public modalManager = inject(ModalManager);
   private documents = inject(Documents);
-  public folder_name = '';
+  public file_name = '';
+  public file_content = '';
   public input_errors: any = {};
 
-  public createFolder(): void {
+  public createFile(): void {
     let parent_id = null;
     const selected_folder = this.documents.getSelectedFolder();
-    if (selected_folder && selected_folder.file_id != null) {
-      parent_id = selected_folder.file_id;
+    if (selected_folder && selected_folder.document_id != null) {
+      parent_id = selected_folder.document_id;
     }
 
     this.input_errors = {};
-    if (this.folder_name == '') {
+    if (this.file_name == '') {
       this.input_errors['name'] = this.l.s('form.required');
       return;
     }
 
     this.http.post(
-      `${Config.API_URL}/v1/documents/new_folder`,
-      { name: this.folder_name, parent_id },
+      `${Config.API_URL}/v1/documents/new_file`,
+      { name: this.file_name, parent_id, content: this.file_content },
       { withCredentials: true }
     )
     .subscribe((data: any) => {
       if ('error' in data) {
         switch(data.error) {
-          case 'folder_already_created':
-            this.input_errors['name'] = this.l.s('documents.folder_already_created')
+          case 'file_already_created':
+            this.input_errors['name'] = this.l.s('documents.file_already_created')
             break;
         }
       }
@@ -51,17 +52,17 @@ export class CreateFileComponent {
       this.documents.addFile({
         document_id: data.document_id,
         file_id: data.file_id,
-        file_uuid: null,
-        file_format: null,
-        mime_type: '',
+        file_uuid: data.file_uuid,
+        file_format: data.file_format,
+        mime_type: data.mime_type,
         parent_id: data.parent_id,
         name: data.name,
-        type: 'folder',
+        type: 'file',
         modified_at: data.created_at,
         created_at: data.created_at,
         owner_id: data.owner_id,
         permissions: [],
-        file_size: 0
+        file_size: data.file_size
       })
     })
   }
